ee658f31e1cc87f7bf467b924a1459fc439adb91
Samuel König de Oliveira <skoliveira@users.noreply.github.com>
2023-09-30 01:44:00 -0300

    first commit

---
+++ src/events/ready.js
@@ -0,0 +1,10 @@
+import { Events } from 'discord.js';
+
+// When the client is ready, run this code (only once)
+export default {
+	name: Events.ClientReady,
+	once: true,
+	execute(client) {
+		console.log(`Ready! Logged in as ${client.user.tag}`);
+	},
+};
---
+++ src/index.js
@@ -0,0 +1,51 @@
+import { Client, Collection, GatewayIntentBits } from 'discord.js';
+import config from './config.js';
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+
+// Get the directory name of the current module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// Command handling
+client.commands = new Collection();
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		}
+		else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
+
+// Event handling
+(async (c) => {
+	const eventsPath = path.join(__dirname, 'events');
+	const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+	for (const file of eventFiles) {
+		const filePath = path.join(eventsPath, file);
+		const { default:event } = await import(filePath);
+		if (event.once) {
+			c.once(event.name, (...args) => event.execute(...args));
+		}
+		else {
+			c.on(event.name, (...args) => event.execute(...args));
+		}
+	}
+})(client);
+
+// Log in to Discord with your client's token
+client.login(config.DISCORD_TOKEN);
